6be3fee259dacfe097a7929c547613491a92407f
Ye Yint Aung <yeyintaung.dev@gmail.com>
2024-06-29 18:21:04 +0800

    initial commit

---
+++ src/test/java/com/yya/trivyfilesystemscan/TrivyfilesystemscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yya.trivyfilesystemscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivyfilesystemscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
